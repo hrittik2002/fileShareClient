e043e8bacaf0b4e67b13caf955f169a99bdf70f0
Hrittik <hrittikgarain@gmail.com>
2023-05-07 13:42:21 +0530

    updated

--- src/apiCalls/apiCalls.jsx
+++ src/apiCalls/apiCalls.jsx
@@ -4,7 +4,7 @@ export const uploadFileApiCall = async(file) => {
     try{
         const formData = new FormData();
         formData.append("myfile" , file);
-        const res = await axios.post(`http://localhost:5000/api/files`,
+        const res = await axios.post(`https://file-share-rest-api.vercel.app/api/files`,
         formData
         )
         console.log(res);
@@ -16,7 +16,7 @@ export const uploadFileApiCall = async(file) => {
 }
 export const sendEmailApiCall = async(uuid , fromEmail , toEmail) =>{
     try{
-        const res = await axios.post(`http://localhost:5000/api/files/send`,
+        const res = await axios.post(`https://file-share-rest-api.vercel.app/api/files/send`,
         {
             uuid : uuid,
             emailTo : toEmail,
@@ -32,7 +32,7 @@ export const sendEmailApiCall = async(uuid , fromEmail , toEmail) =>{
 }
 export const getDownloadDeltailsApiCall = async(uuid) =>{
     try{
-        const res = await axios.get(`http://localhost:5000/api/show/${uuid}`);
+        const res = await axios.get(`https://file-share-rest-api.vercel.app/api/show/${uuid}`);
         console.log(res);
         return res;
     }
